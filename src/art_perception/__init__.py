22e8a689a5b9c15b127e5d624a9c8f6f5f12da08
David Richards <davidlamontrichards@gmail.com>
2025-05-05 17:30:54 -0600

    wip

--- src/art_perception/__init__.py
+++ src/art_perception/__init__.py
@@ -1,5 +1,11 @@
 """Top-level package for Art Perception."""
 
 __author__ = """David Richards"""
-__email__ = 'davidlamontrichards@gmail.com'
-__version__ = '0.1.0'
+__email__ = "davidlamontrichards@gmail.com"
+__version__ = "0.1.0"
+
+__all__ = []
+
+from .domain import extract_palette
+
+__all__.append("extract_palette")
